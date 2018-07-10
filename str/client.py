d645655c3c173d27164c7d7cf8b289d40d277750
eatPorkAndSeePigRun <811302964@qq.com>
2018-07-10 20:43:03 +0800

    单线程c-s

--- str/client.py
+++ str/client.py
@@ -1,30 +1,25 @@
 import socket
+import struct
+
+
+def socket_send_n(socket, data):
+    hasSize = 0
+    dataSize = len(data)
+    while not hasSize >= dataSize:
+        hasSize += socket.send(data[hasSize:hasSize + 1024])
+
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.connect(("127.0.0.1", 8080))
     print("***客户端即将启动，正在连接服务器端***")
-    for i in range(3):  # 发送3张图
+    for pic in ["1.jpg", "2.jpg", "3.jpg"]:  # 发送3张图
         # 文件长度
-        file = open(str(i + 1) + ".jpg", "rb")
-        fileContent = file.read()
-        file.close()
+        with open(pic, "rb") as file:
+            fileContent = file.read()
         fileSize = len(fileContent)
-        print("我是客户端，要发送" + str(i + 1) + ".jpg的大小为：", end="")
-        print(fileSize)
-        s.send(bytes(str(fileSize), encoding="utf-8"))
+        print("我是客户端，要发送" + pic + "的大小为：", fileSize)
+        socket_send_n(s, struct.pack("i", fileSize))
+
         # 文件内容
-        data = s.recv(1024)
-        if data == b'start':
-            print(str(i + 1) + ".jpg开始发送。")
-            hasSize = 0
-            while True:
-                if hasSize + 1024 <= fileSize:
-                    hasSize += s.send(fileContent[hasSize:hasSize + 1024])
-                else:
-                    hasSize += s.send(fileContent[hasSize:fileSize])
-                    print(str(i + 1) + ".jpg发送完毕。")
-                    print("总共发送的大小：", end="")
-                    print(hasSize)
-                    if s.recv(1024) == b'end':
-                        break
-    s.close()
+        socket_send_n(s, fileContent)
+        print(pic + "发送完成。")
--- str/server.py
+++ str/server.py
@@ -1,4 +1,14 @@
 import socket
+import struct
+
+
+def socket_recv_n(socket, n):
+    data = b''
+    while len(data) + 1024 <= n:
+        data += socket.recv(1024)
+    data += socket.recv(n - len(data))
+    return data
+
 
 # socket.AF_INET --> 机器网络之间的通信
 # socket.SOCK_STREAM --> TCP协议通信
@@ -8,30 +18,14 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     conn, addr = s.accept()  # 调用accept()方法开始监听，等待客户端的连接
     with conn:
         print("***服务器即将启动，等待客户端的连接***")
-        for i in range(3):  # 接收3张图
+        for pic in ["1.jpg", "2.jpg", "3.jpg"]:  # 接收3张图
             # 文件长度
-            data = conn.recv(1024)
-            fileSize = int(str(data, encoding="utf-8"))
-            print("我是服务器端，要接收" + str(i + 1) + ".jpg的大小为：", end="")
-            print(fileSize)
+            data = socket_recv_n(conn, 4)
+            fileSize = struct.unpack("i", data)[0]
+            print("我是服务器端，要接收" + pic + "的大小为：", fileSize)
 
             # 文件内容
-            conn.send(b'start')
-            print(str(i + 1) + ".jpg开始接收。")
-            hasSize = 0
-            file = open(str(i + 1) + "server.jpg", "wb")
-            while True:
-                if hasSize + 1024 <= fileSize:
-                    data = conn.recv(1024)
-                    file.write(data)
-                    hasSize += len(data)
-                else:
-                    data = conn.recv(1024)
-                    file.write(data)
-                    file.close()
-                    print(str(i + 1) + ".jpg接收完成。")
-                    print("总共接收的大小：", end="")
-                    print(hasSize + len(data))
-                    conn.send(b'end')
-                    break
-    conn.close()
+            data = socket_recv_n(conn, fileSize)
+            with open("server" + pic, "wb") as file:
+                file.write(data)
+            print(pic + "接收完成，总共接收的大小：", len(data))
