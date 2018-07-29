827ebf484ebff46c0217b504abc18c517ba58f7d
eatPorkAndSeePigRun <811302964@qq.com>
2018-07-29 21:51:32 +0800

    nonblock select echo

--- str/client.py
+++ str/client.py
@@ -1,79 +1,9 @@
-import time
 import socket
-import threading
-from log import *
 
-
-class ChatClient:
-    def __init__(self, ip, port):
-        self.ip = ip
-        self.port = port
-        self.opened = False
-        log("ChatClient __init__")
-
-    def open(self):
-        self.opened = True
-        log("ChatClient open")
-        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.socket.connect((self.ip, self.port))
-        self.recv_content_thread = threading.Thread(target=self.recv_content_loop)
-        self.recv_content_thread.start()
-        self.send_content_loop()
-
-    def close(self):
-        if not self.opened:
-            return
-        log("ChatClient close")
-        self.opened = False
-        self.socket.close()
-
-    def recv_content_loop(self):
-        log("ChatClient recv_loop start")
-        while self.opened:
-            try:
-                msg = str(self.socket.recv(1024), encoding="utf-8")
-                print("\n<<<", msg)
-                print(">>>")
-            except socket.error as e:
-                log("ChatClient recv_content_loop exception %s %s" % (e, self))
-                break
-        log("ChatClient recv_loop end")
-
-    def send_content_loop(self):
-        log("ChatClient send_loop start")
-        while self.opened:
-            try:
-                content = input(">>>")
-                self.send_n(bytes(content, encoding="utf-8") + b'\x00')
-            except KeyboardInterrupt as e:
-                break
-        log("ChatClient send_loop end")
-
-    def send_n(self, data):
-        if data == b'':
-            return
-        log("ChatClient send_n %s %s" % (data, self))
-        hasSize = 0
-        dataSize = len(data)
-        while not hasSize >= dataSize:
-            try:
-                hasSize += self.socket.send(data[hasSize:hasSize + 1])
-                time.sleep(1)
-            except socket.timeout:
-                continue
-
-
-def main():
-    chatClient = ChatClient("127.0.0.1", 8080)
-    print("***客户端即将启动，正在连接服务器端***")
-    chatClient.open()
-    while True:
-        try:
-            time.sleep(1)
-        except KeyboardInterrupt as e:
-            break
-    chatClient.close()
-
-
-main()
-log("client main thread exit")
+s = socket.socket()
+s.connect(("127.0.0.1", 8080))
+while True:
+    msg = bytes(input("发送："), encoding="utf-8")
+    s.send(msg)
+    data = s.recv(1024)
+    print('收到：', str(data, encoding="utf-8"))
--- str/server.py
+++ str/server.py
@@ -1,75 +1,46 @@
-import time
-from tcp_server import *
+import socket
+import select
+import queue
 
-
-class ChatServer:
-    def __init__(self):
-        self.tcp_server = TcpServer("127.0.0.1", 8080, self.on_connect, self.on_disconnect, self.on_recv_data)
-        self.rooms = {}
-        self.clientSayInRoom = {}
-        self.clientData = {}
-
-    def on_connect(self, tcp_connection):
-        pass
-
-    def on_disconnect(self):
-        pass
-
-    def on_recv_data(self, tcp_connection, data):
-        if data == b'':
-            return
-        if tcp_connection not in self.clientData:
-            self.clientData[tcp_connection] = b''
-        if data.count(b'\x00') != 0:
-            index = data.index(b'\x00')
-            self.clientData[tcp_connection] += data[0: index]
-            # 处理
-            if str(self.clientData[tcp_connection], encoding="utf-8") == "listroom":
-                rooms = " ".join(self.rooms.keys())
-                tcp_connection.send_binary(bytes(rooms, encoding="utf-8"))
-                return
-            try:
-                command, content = str(self.clientData[tcp_connection], encoding="utf-8").split()
-                if command == "createroom":
-                    self.rooms[content] = []
-                elif command == "deleteroom":
-                    del self.rooms[content]
-                elif command == "joinroom":
-                    self.rooms[content].append(tcp_connection)
-                elif command == "quitroom":
-                    self.rooms[content].remove(tcp_connection)
-                elif command == "sayroom":
-                    self.clientSayInRoom[tcp_connection] = content
-            except ValueError:
-                for i in self.rooms[self.clientSayInRoom[tcp_connection]]:
-                    if i != tcp_connection and i is not str:
-                        i.send_binary(self.clientData[tcp_connection])
-            except BaseException as error:
-                print(error)
-            # 递归
-            self.clientData[tcp_connection] = b''
-            self.on_recv_data(tcp_connection, data[index + 1: len(data)])
+server = socket.socket()
+server.setblocking(False)
+server.bind(("127.0.0.1", 8080))
+server.listen()
+rlist = [server]
+wlist = []
+msg_queues = {}
+while True:
+    readable, writable, exceptional = select.select(rlist, wlist, rlist)
+    for s in readable:
+        if s is server:
+            conn, _ = s.accept()
+            conn.setblocking(False)
+            rlist.append(conn)
+            msg_queues[conn] = queue.Queue()
         else:
-            self.clientData[tcp_connection] += data
-
-    def open(self):
-        self.tcp_server.open()
-
-    def close(self):
-        self.tcp_server.close()
-
-
-def main():
-    chatServer = ChatServer()
-    print("***服务器即将启动，等待客户端的连接***")
-    chatServer.open()
-    while True:
+            try:
+                data = s.recv(1024)
+            except ConnectionResetError:
+                rlist.remove(s)
+                if s in wlist:
+                    wlist.remove(s)
+                s.close()
+                del msg_queues[s]
+            else:
+                if data:
+                    msg_queues[s].put(data)
+                    if s not in wlist:
+                        wlist.append(s)
+    for s in writable:
         try:
-            time.sleep(1)
-        except KeyboardInterrupt as e:
-            break
-    chatServer.close()
-
-
-main()
-log("server main thread exit")
+            next_msg = msg_queues[s].get_nowait()
+        except queue.Empty:
+            wlist.remove(s)
+        else:
+            s.send(next_msg)
+    for s in exceptional:
+        rlist.remove(s)
+        if s in wlist:
+            wlist.remove(s)
+        s.close()
+        del msg_queues[s]
